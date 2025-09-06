ba651051d34528769be0c4afbe1d058201bbfd66
mernrehandev-cloud <mernrehandev@gmail.com>
2025-09-07 01:44:13 +0500

    Update login.jsx

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,12 +1,20 @@
+import { useEffect, useState } from "react";
 import LoginForm from "../components/login/loginform";
 import SecHeader from "../components/secondaryheader/secheader";
 
-export default function Login({ BEurl }) {
+export default function Login({ BEurl, users }) {
+    const [keyforut, setkeyforut] = useState(null);
+
+    useEffect(() => {
+        if (users) {
+            setkeyforut("u");
+        }
+    }, []);
+
     return (
         <>
-            <SecHeader heading={"Login Page"} icon_nav="fa fa-right-to-bracket" />
+            <SecHeader heading={"Login Page"} icon_nav="fa fa-right-to-bracket" total={users.length} keyforut={keyforut} />
             <LoginForm BEurl={BEurl} />
         </>
     )
-
 }
