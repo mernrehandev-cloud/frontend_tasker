6538114eaf6d844f92c56e822b45fa635aa74e4c
mernrehandev-cloud <mernrehandev@gmail.com>
2025-09-07 01:44:32 +0500

    Update register.jsx

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,12 +1,19 @@
-import SignupForm from "../components/signup/signupform";
+import { useEffect, useState } from "react";
 import SecHeader from "../components/secondaryheader/secheader";
+import SignupForm from "../components/signup/signupform";
 
-export default function Register() {
+export default function Register({ BEurl, users }) {
+    const [keyforut, setkeyforut] = useState(null);
+
+    useEffect(() => {
+        if (users) {
+            setkeyforut("u");
+        }
+    }, []);
     return (
         <>
-            <SecHeader heading={"Sign Up Page"} icon_nav="fa fa-user-plus" />
-            <SignupForm />
+            <SecHeader heading={"Sign Up Page"} icon_nav="fa fa-user-plus" total={users.length} keyforut={keyforut} />
+            <SignupForm BEurl={BEurl} />
         </>
     )
-
 }
