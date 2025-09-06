76ae9522c37462e929036ddc4de3a3e6e91a0902
mernrehandev-cloud <mernrehandev@gmail.com>
2025-09-07 00:58:44 +0500

    Update register.jsx

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,9 +1,12 @@
 import SignupForm from "../components/signup/signupform";
+import SecHeader from "../components/secondaryheader/secheader";
 
 export default function Register() {
     return (
         <>
+            <SecHeader heading={"Sign Up Page"} icon_nav="fa fa-user-plus" />
             <SignupForm />
         </>
     )
+
 }
