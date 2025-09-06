f965b4b640358701de9cb76f879c4efd37016522
mernrehandev-cloud <mernrehandev@gmail.com>
2025-09-07 00:57:38 +0500

    Update login.jsx

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,9 +1,12 @@
 import LoginForm from "../components/login/loginform";
+import SecHeader from "../components/secondaryheader/secheader";
 
 export default function Login({ BEurl }) {
     return (
         <>
+            <SecHeader heading={"Login Page"} icon_nav="fa fa-right-to-bracket" />
             <LoginForm BEurl={BEurl} />
         </>
     )
+
 }
